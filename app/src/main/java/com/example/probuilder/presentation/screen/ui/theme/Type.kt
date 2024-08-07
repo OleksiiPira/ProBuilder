0ed0b0fa6b6139fecdda0ded88dd297464c86b76
Oleksii <oleksii.pira@accedo.tv>
2024-08-07 17:25:37 +0200

    update fonts

--- app/src/main/java/com/example/probuilder/presentation/screen/ui/theme/Type.kt
+++ app/src/main/java/com/example/probuilder/presentation/screen/ui/theme/Type.kt
@@ -2,78 +2,88 @@ package com.example.probuilder.presentation.screen.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+//import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.probuilder.R.font as FontStyle
 
+val nunitoFamily = FontFamily(
+    Font(FontStyle.nunito_regular, FontWeight.Normal),
+    Font(FontStyle.nunito_bold, FontWeight.Bold),
+    Font(FontStyle.nunito_light, FontWeight.Light),
+    Font(FontStyle.nunito_medium, FontWeight.Medium),
+    Font(FontStyle.nunito_semi_bold, FontWeight.SemiBold),
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     //    Title
     titleLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
+        fontFamily = nunitoFamily,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
-        lineHeight = 66.sp,
+        lineHeight = 28.sp,
         letterSpacing = 0.sp,
     ),
     titleMedium = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
+        fontFamily = nunitoFamily,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 18.sp,
-        lineHeight = 28.sp,
+        lineHeight = 25.sp,
         letterSpacing = 0.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
-        lineHeight = 20.sp,
+        fontFamily = nunitoFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
         letterSpacing = 0.sp
     ),
 
     //    Body
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
+        fontFamily = nunitoFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 19.sp,
         letterSpacing = 0.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.SansSerif,
+        fontFamily = nunitoFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
-        lineHeight = 20.sp,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
         letterSpacing = 0.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 9.sp,
-        lineHeight = 16.sp,
+        fontFamily = nunitoFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 10.sp,
+        lineHeight = 14.sp,
         letterSpacing = 0.sp
     ),
 
     //    Label
     labelLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 18.sp,
-        lineHeight = 24.sp,
+        fontFamily = nunitoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
         letterSpacing = 0.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
+        fontFamily = nunitoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = nunitoFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 9.sp,
-        lineHeight = 16.sp,
+        lineHeight = 12.sp,
         letterSpacing = 0.5.sp
     )
 )
