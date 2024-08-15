e9d1ae844f1beb41c02f56e3688266cb6afb98d5
Oleksii <oleksii.pira@accedo.tv>
2024-08-15 17:51:48 +0200

    update state

--- app/src/main/java/com/example/probuilder/presentation/screen/categories/create_job/CreateJobState.kt
+++ app/src/main/java/com/example/probuilder/presentation/screen/categories/create_job/CreateJobState.kt
@@ -2,5 +2,6 @@ package com.example.probuilder.presentation.screen.categories.create_job
 
 data class CreateJobState(
     val pricePerUnit: String = "",
-    var isEditMode: Boolean = false
+    var isEditMode: Boolean = false,
+    var tags: List<String> = emptyList()
 )
