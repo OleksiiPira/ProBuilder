32ef5703a83e7eb436bfab40b5d381e077bf26f6
Oleksii <oleksii.pira@accedo.tv>
2024-08-23 21:00:57 +0200

    add text composables

---
+++ app/src/main/java/com/example/probuilder/presentation/components/Text.kt
@@ -0,0 +1,47 @@
+package com.example.probuilder.presentation.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import com.example.probuilder.presentation.screen.ui.theme.Typography
+
+@Composable
+fun TitleLarge(text: String = "", modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.titleLarge, modifier = modifier)
+}
+@Composable
+fun TitleMedium(text: String = "", modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.titleMedium, modifier = modifier)
+}
+@Composable
+fun TitleSmall(text: String = "", modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.titleSmall, modifier = modifier)
+}
+@Composable
+fun BodyLarge(text: String = "", modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.bodyLarge, modifier = modifier)
+}
+@Composable
+fun BodyMedium(text: String = "", fontWeight: FontWeight = FontWeight.Normal, modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.bodyMedium, modifier = modifier)
+}
+@Composable
+fun BodySmall(text: String = "", color: Color = Color.Black, modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.bodySmall, modifier = modifier, color = color)
+}
+@Composable
+fun LabelLarge(text: String = "", modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.labelLarge, modifier = modifier)
+}
+@Composable
+fun LabelMedium(text: String = "", modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.labelMedium, modifier = modifier)
+}
+@Composable
+fun LabelSmall(text: String = "", modifier: Modifier = Modifier) {
+    Text(text = text, style = Typography.labelSmall, modifier = modifier)
+}
+
+
