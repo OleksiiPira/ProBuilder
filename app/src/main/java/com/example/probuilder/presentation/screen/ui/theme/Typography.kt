1238a6fc8858014536635581bd9bdad588c5c10c
Oleksii <oleksii.pira@accedo.tv>
2024-09-09 20:20:12 +0200

    fix typo

--- app/src/main/java/com/example/probuilder/presentation/screen/ui/theme/Typography.kt
+++ app/src/main/java/com/example/probuilder/presentation/screen/ui/theme/Typography.kt
@@ -3,7 +3,6 @@ package com.example.probuilder.presentation.screen.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
-//import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
